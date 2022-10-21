374641cacc52ec043dd93e0c073a933034ab4d5d
color <95304946+HermanMazur@users.noreply.github.com>
2022-10-21 11:09:12 +0300

    repeta react 1 components
    repeta react 1 components

---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Section({ title, children }) {
+  return (
+    <div>
+      {title && <h2>{title}</h2>}
+      {children}
+    </div>
+  );
+}
+
+Section.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Section;
